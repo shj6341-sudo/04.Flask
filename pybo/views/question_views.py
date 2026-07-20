2dcf761a71a9efbb295af52df5f027de2ae04cbc
HyunJun <shj6341@gmail.com>
2026-07-20 17:55:47 +0900

    설명 주석 추가

--- pybo/views/question_views.py
+++ pybo/views/question_views.py
@@ -10,6 +10,8 @@ def _list():
 
     # render_template ==> html 파일을 불러오는 역할
     return render_template('question/question_list.html', question_list=question_list, datetime=datetime)
+    # import 해온 모듈(datetime 등)을 HTML 안에서 사용하려면 키워드 인자로 전달해야 함
+    # question_list는 화면에 보여줄 '데이터'라서 키워드 인자로 넘겨주는 것
 
 @bp.route('/detail/<int:question_id>/')
 def detail(question_id):
