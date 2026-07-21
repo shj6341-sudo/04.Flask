d9e2385734cfcaaa3f07783e883bf3c0b4daa224
HyunJun <shj6341@gmail.com>
2026-07-21 18:01:51 +0900

    7/21 수업

--- config.py
+++ config.py
@@ -4,3 +4,6 @@ BASE_DIR = os.path.dirname(__file__)
 
 SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# CSRF 토큰 생성을 위한 비밀키 추가
+SECRET_KEY = "TEN#TKkdktp@enetTTneO03T"
